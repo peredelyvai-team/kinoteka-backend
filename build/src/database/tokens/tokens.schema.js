deb189aea513fb8c0ab67405c713251ace2a33c1
DenisGridin <denisgridin6@gmail.com>
2021-03-02 17:32:50 +0300

    remove unnessesary files and directories && git push

--- build/src/database/tokens/tokens.schema.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const TokenSchema = new mongoose_1.Schema({
-    token: String
-});
-exports.default = TokenSchema;
--- build/src/utils/helpers.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.errorHandler = void 0;
-function errorHandler(error, res, code = 500) {
-    console.log(error);
-    res.status(500).send(error);
-}
-exports.errorHandler = errorHandler;
